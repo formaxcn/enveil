92b0c83f5d7c605c9fd205618d92c9bf515156bb
TangZhihao <tjrtzh@gmail.com>
2025-12-02 05:28:17 +0800

    text

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,17 +1,17 @@
 import { defineConfig } from 'wxt';
 
-// See https://wxt.dev/api/config.html
 export default defineConfig({
   manifest: {
     name: "Enveil",
     version: "1.0.0",
-    description: "A basic extension with popup and options pages",
+    description:
+      "A extension for Devops to help you identify your differences between your environments.",
     icons: {
       "16": "icon/16.png",
       "32": "icon/32.png",
       "48": "icon/48.png",
       "96": "icon/96.png",
-      "128": "icon/128.png"
+      "128": "icon/128.png",
     },
-  }
+  },
 });
