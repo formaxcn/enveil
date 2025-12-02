9b025f6477db621a34ff1e5a51b85b27541d3a1a
formaxcn <tjrtzh@gmail.com>
2025-12-02 13:58:55 +0800

    using wxt

---
+++ entrypoints/options/main.ts
@@ -0,0 +1,5 @@
+import './style.css';
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('Options page loaded');
+});
---
+++ wxt.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'wxt';
+
+// See https://wxt.dev/api/config.html
+export default defineConfig({
+  manifest: {
+    name: "My Extension",
+    version: "1.0.0",
+    description: "A basic extension with popup and options pages"
+  }
+});
